8cdf89802fdaa02c320e9123f529dd772fdf990a
Denpa_Ghost <az.kinosaki@gmail.com>
2021-02-07 22:38:09 +0900

    [add]タマクラF

--- src/pages/events/index.vue
+++ src/pages/events/index.vue
@@ -95,9 +95,11 @@
         </div>
 
         <div class="col-12 col-lg-6 mb-3 text-center">
-          <img src="/img/logos/tmkr_nazo.png"
-               alt="?"
-               class="img-fluid"/>
+          <nuxt-link to="/events/tmkrf">
+            <img src="/img/logos/tmkrF_logo.png"
+                 alt="タマクラF"
+                 class="img-fluid"/>
+          </nuxt-link>
         </div>
 
         <div class="col-12 col-lg-6 mb-3 text-center">
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -12,13 +12,13 @@
       <div class="row">
         <div class="col-12 pb-3">
           <h1 class="font-expletus text-hi index-shadow-rb-sm">
-            タマクラ4.5 開催！
+            タマクラF 開催！
           </h1>
           <p>
-            ── DJのタマゴ、解放 ───
+            ── DJのタマゴ、孵化 ───
           </p>
           <div class="text-right">
-            <tama-button to="/events/tmkr04-5" nuxt-link>
+            <tama-button to="/events/tmkrf" nuxt-link>
               show more
             </tama-button>
           </div>
@@ -99,7 +99,7 @@ export default {
         },
         {
           property: 'og:image',
-          content: `${process.env.BASE_URL}/img/fliers/tmkr04-5.jpg`,
+          content: `${process.env.BASE_URL}/img/fliers/tmkrf.jpg`,
         },
         {
           property: 'og:description',
